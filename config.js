8b651ca6033a1550a63d2154d90b8fdcf2043f22
Vithop <vithuransoccer1@gmail.com>
2019-09-09 17:10:18 -0400

    Added: login page

---
+++ config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-2_Qo1ayCM3X', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '163qko7ee4iqa28372itngf0oo', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
--- src/App.js
+++ src/App.js
@@ -1,86 +1,80 @@
-import React, { Component } from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
-import Dashboard from './components/layout/Dashboard';
-import Gnome from './components/gnomes/Gnome';
-import SideNav, { NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import Login from "./components/login/Login";
+import Dashboard from "./components/layout/Dashboard";
+import Gnome from "./components/gnomes/Gnome";
+import SideNav, { NavItem, NavIcon, NavText } from "@trendmicro/react-sidenav";
 
 // Be sure to include styles at some point, probably during your bootstraping
-import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+import "@trendmicro/react-sidenav/dist/react-sidenav.css";
 
-import './App.css';
-import backgroundImg from './assets/bg-white-01.jpg';
+import "./App.css";
+import backgroundImg from "./assets/bg-white-01.jpg";
 //import backgroundImg from './assets/bg-green.jpg';
 //import backgroundImg from './assets/bg-light.jpg';
 //import backgroundImg from './assets/Artboard-1.png';
 //import backgroundImg from './assets/bg.jpg';
 
 class App extends Component {
-	showSettings(event) {
-		event.preventDefault();
-	}
-	render() {
-		return (
-			<Router>
-				<Route
-					render={({ location, history }) => (
-						<React.Fragment>
-							<SideNav
-								onSelect={selected => {
-									const to = '/' + selected;
-									if (location.pathname !== to) {
-										history.push(to);
-									}
-								}}
-							>
-								<SideNav.Toggle />
-								<SideNav.Nav defaultSelected="home">
-									<NavItem eventKey="">
-										<NavIcon>
-											<i
-												className="fa fa-fw fa-home"
-												style={{ fontSize: '1.75em' }}
-											/>
-										</NavIcon>
-										<NavText>Home</NavText>
-									</NavItem>
-								</SideNav.Nav>
-							</SideNav>
-							<div
-								className="App"
-								style={{
-									backgroundImage: `url(${backgroundImg})`,
-									paddingLeft: '6rem',
-									paddingRight: '2rem'
-								}}
-							>
-								<div
-									className="container"
-									style={{
-										paddingTop: '4rem',
-										paddingBottom: '4rem'
-									}}
-								>
-									<Switch>
-										<Route
-											exact
-											path="/"
-											component={Dashboard}
-										/>
-										<Route
-											exact
-											path="/gnome/:gnomeindex"
-											component={Gnome}
-										/>
-									</Switch>
-								</div>
-							</div>
-						</React.Fragment>
-					)}
-				/>
-			</Router>
-		);
-	}
+  showSettings(event) {
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <Router>
+        <Route
+          render={({ location, history }) => (
+            <React.Fragment>
+              <SideNav
+                onSelect={selected => {
+                  const to = "/" + selected;
+                  if (location.pathname !== to) {
+                    history.push(to);
+                  }
+                }}
+              >
+                <SideNav.Toggle />
+                <SideNav.Nav defaultSelected="home">
+                  <NavItem eventKey="">
+                    <NavIcon>
+                      <i
+                        className="fa fa-fw fa-home"
+                        style={{ fontSize: "1.75em" }}
+                      />{" "}
+                    </NavIcon>{" "}
+                    <NavText> Home </NavText>{" "}
+                  </NavItem>{" "}
+                </SideNav.Nav>{" "}
+              </SideNav>{" "}
+              <div
+                className="App"
+                style={{
+                  backgroundImage: `url(${backgroundImg})`,
+                  paddingLeft: "6rem",
+                  paddingRight: "2rem"
+                }}
+              >
+                <div
+                  className="container"
+                  style={{
+                    paddingTop: "4rem",
+                    paddingBottom: "4rem"
+                  }}
+                >
+                  <Switch>
+                    <Route exact path="/login" component={Login} />{" "}
+                    <Route exact path="/" component={Dashboard} />{" "}
+                    <Route exact path="/gnome/:gnomeindex" component={Gnome} />{" "}
+                  </Switch>{" "}
+                </div>{" "}
+              </div>{" "}
+            </React.Fragment>
+          )}
+        />{" "}
+      </Router>
+    );
+  }
 }
 
 export default App;
