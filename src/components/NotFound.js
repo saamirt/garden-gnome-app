02721e4b63f615db975da1115ba6974a9d846395
Vithop <vithuransoccer1@gmail.com>
2019-09-17 09:48:58 -0400

    Added : go back to login/home for notfound

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,7 +1,34 @@
 import React from "react";
+import {Button, Jumbotron} from "react-bootstrap";
 
-export default () => (
+export default (childProps) => (
     <div>
-        <h1>Page Not Found</h1>
+        <Jumbotron>
+            <h1>Page Not Found</h1>
+            <p>
+                Ooops! Seems like you ended up in the wrong place. Maybe use the gnomes to find your way back!
+            </p>
+            <p>
+                <Button 
+                    variant="primary"
+                    className={'RedirectLoginButton'}
+                    size="lg"
+                    onClick={()=>childProps.history.push("/login")}
+                >
+                    Go back to login
+                </Button>
+            </p>
+            <p>
+                <Button 
+                    variant="success"
+                    className={'RedirectHomeButton'}
+                    size="lg"
+                    onClick={()=>childProps.history.push("/home")}
+                >
+                    Go back home
+                </Button>
+            </p>
+            //TODO:Add lost gnome picture
+        </Jumbotron>
     </div>
   );
